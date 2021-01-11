322a1d0c99375bd434bed391f95ea4344ddc6929
akashgajwar <75195665+akashgajwar@users.noreply.github.com>
2021-01-12 00:01:36 +0530

    core : logic fixed.

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ function clickHandler(){
     var lno = luckeyNo.value;
     var date = [];
     var total = 0;
-    for(i=0; i<9; i++){
-        date[i] = hbd.substring((i), (i+1));
-        // console.log(date);
+    var per = 0;
+    var sum = 0;
+
+    for(i=0; i<8; i++){
+        per = parseInt(hbd.substring((i), (i+1)));
+        sum = sum + per;   
     }
-    for (i=0; i<9; i++){
-        total= total + date[i];
+
+    if(sum % lno ===0 ){
+        var result = "you are born on a luckey day";
+    }else{
+        var result = "You are not born on a luckey day";
     }
-    console.log(total);
-    console.log("your luckey no. is "+lno);
+
+    outputDiv.innerText = result;
 }
 
 
